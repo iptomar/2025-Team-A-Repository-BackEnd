783919eed0d43dbeb6884e843ca2b6abe21a3126
TiagoMatos23026 <128063394+TiagoMatos23026@users.noreply.github.com>
2025-03-21 15:26:43 +0000

    Criação de Classe para User e adição de serviços necessários

--- Program.cs
+++ Program.cs
@@ -10,11 +10,20 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddRazorPages();
+
+//Serviços para autenticação
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
 var app = builder.Build();
+
+
+app.MapIdentityApi<IdentityUser>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
