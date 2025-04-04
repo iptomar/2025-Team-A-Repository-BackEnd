b10d1ff277fe4b43043f8661616b91cf0495d0d8
TiagoMatos23026 <128063394+TiagoMatos23026@users.noreply.github.com>
2025-04-04 17:38:23 +0100

    Criação de controller com endpoints por defeito

---
+++ Controllers/API_HorariosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GP_Backend.Data;
+using GP_Backend.Models;
+
+namespace GP_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class API_HorariosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public API_HorariosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/API_Horarios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Horarios>>> GetHorarios()
+        {
+            return await _context.Horarios.ToListAsync();
+        }
+
+        // GET: api/API_Horarios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Horarios>> GetHorarios(int id)
+        {
+            var horarios = await _context.Horarios.FindAsync(id);
+
+            if (horarios == null)
+            {
+                return NotFound();
+            }
+
+            return horarios;
+        }
+
+        // PUT: api/API_Horarios/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHorarios(int id, Horarios horarios)
+        {
+            if (id != horarios.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(horarios).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HorariosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/API_Horarios
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Horarios>> PostHorarios(Horarios horarios)
+        {
+            _context.Horarios.Add(horarios);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetHorarios", new { id = horarios.Id }, horarios);
+        }
+
+        // DELETE: api/API_Horarios/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHorarios(int id)
+        {
+            var horarios = await _context.Horarios.FindAsync(id);
+            if (horarios == null)
+            {
+                return NotFound();
+            }
+
+            _context.Horarios.Remove(horarios);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool HorariosExists(int id)
+        {
+            return _context.Horarios.Any(e => e.Id == id);
+        }
+    }
+}
