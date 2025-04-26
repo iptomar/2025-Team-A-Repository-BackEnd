b8936c24db2ad11bc774550eff2a57f50c94981c
Aluno24845 <aluno24845@ipt.pt>
2025-04-26 18:39:22 +0100

    Alterado o modelo Utilizadores para remover a herança de IdentityUser e introduzido o campo UserId para ligação com o sistema de autenticação.
    Removidos os campos Email, Password e Role. Atualizados os campos CursoFK e EscolaFK para serem opcionais. Feitas alterações no ApplicationDbContext para refletir estas mudanças.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -8,7 +8,7 @@ namespace GP_Backend.Data;
 /// <summary>
 /// classe responsável pela criação e gestão da Base de dados
 /// </summary>
-public class ApplicationDbContext : IdentityDbContext<Utilizadores>
+public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
