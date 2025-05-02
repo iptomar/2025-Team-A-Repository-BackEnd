82daf89d96913ffe87251d26dd17fc28eebd1475
NVital14 <96735261+NVital14@users.noreply.github.com>
2025-05-02 10:16:05 +0100

    Voltar a colocar o relacionamento de 1-M entre as turmas e os horários

--- Models/Turmas.cs
+++ Models/Turmas.cs
@@ -8,18 +8,31 @@ namespace GP_Backend.Models
     /// </summary>
     public class Turmas
     {
+        /// <summary>
+        /// Construtor da classe Turmas
+        /// </summary>
+        public Turmas()
+        {
+            Horarios = new HashSet<Horarios>();
+        }
+
 
         [Key]
         public int Id { get; set; }
 
         public string Nome { get; set; }
 
-        public string AnoLetivo { get; set; }
+        public string AnoCurso { get; set; }
 
         /* ************************************************
          * Vamos criar as Relações (FKs) com outras tabelas
          * *********************************************** */
 
+        /* ************************************************
+         * Relacionamento 1-N com Horarios
+         * Uma turma pode ter vários horários
+         * *********************************************** */
+        public ICollection<Horarios> Horarios { get; set; }
 
         // Relacionamento do tipo N-1
         [ForeignKey(nameof(Curso))]
