70eac253efcd5be0ef19f60d05e966889150bff2
TiagoMatos23026 <128063394+TiagoMatos23026@users.noreply.github.com>
2025-05-02 15:06:44 +0100

    Correção na migração

---
+++ Data/Migrations/20250502133646_FK_User_Autenticacao.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GP_Backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FK_User_Autenticacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
